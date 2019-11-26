c2ad9e7326c235ee31fe6def8148e7c90451f06b
soundharya04 <souns14@gmail.com>
2019-11-26 11:38:56 +0530

    app modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hellooooo</div>;
+  return <div className="App">React JS</div>;
 }
 
 export default App;
